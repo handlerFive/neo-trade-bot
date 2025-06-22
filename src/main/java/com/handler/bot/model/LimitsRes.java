9b93f72b8cf468be28c237b0869d53cf1cf2a69f
handlerFive <raunaksarkar12@gmail.com>
2025-06-23 03:19:46 +0530

    Neo-Bot - Limits Fetching Service and Margin - Added

---
+++ src/main/java/com/handler/bot/model/LimitsRes.java
@@ -0,0 +1,167 @@
+package com.handler.bot.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LimitsRes {
+
+    @JsonProperty("AddPreExpMrgnMisPrsnt")
+    private String addPreExpMrgnMisPrsnt;
+    @JsonProperty("CurExpMrgnNrmlPrsnt")
+    private String curExpMrgnNrmlPrsnt;
+    @JsonProperty("CurPremiumNrmlPrsnt")
+    private String curPremiumNrmlPrsnt;
+    @JsonProperty("RmsPayOutAmt")
+    private String rmsPayOutAmt;
+    @JsonProperty("AdhocMargin")
+    private String adhocMargin;
+    @JsonProperty("MrgnScrpBsktCncPrsnt")
+    private String mrgnScrpBsktCncPrsnt;
+    @JsonProperty("FoPremiumMisPrsnt")
+    private String foPremiumMisPrsnt;
+    @JsonProperty("MrgnVarNrmlPrsnt")
+    private String mrgnVarNrmlPrsnt;
+    @JsonProperty("AmountUtilizedPrsnt")
+    private String amountUtilizedPrsnt;
+    @JsonProperty("SplMrgnMisPrsnt")
+    private String splMrgnMisPrsnt;
+    @JsonProperty("CurSpanMrgnNrmlPrsnt")
+    private String curSpanMrgnNrmlPrsnt;
+    @JsonProperty("AuxRmsCollateral")
+    private String auxRmsCollateral;
+    @JsonProperty("FoUnRlsMtomPrsnt")
+    private String foUnRlsMtomPrsnt;
+    @JsonProperty("AuxNotionalCash")
+    private String auxNotionalCash;
+    @JsonProperty("AuxAdhocMargin")
+    private String auxAdhocMargin;
+    @JsonProperty("DaneLimit")
+    private String daneLimit;
+    @JsonProperty("RmsPayInAmt")
+    private String rmsPayInAmt;
+    @JsonProperty("CurPremiumMisPrsnt")
+    private String curPremiumMisPrsnt;
+    @JsonProperty("MarginWarningPrcntPrsnt")
+    private String marginWarningPrcntPrsnt;
+    @JsonProperty("MrgnVarMisPrsnt")
+    private String mrgnVarMisPrsnt;
+    @JsonProperty("CurRlsMtomPrsnt")
+    private String curRlsMtomPrsnt;
+    @JsonProperty("ExposureMarginPrsnt")
+    private String exposureMarginPrsnt;
+    @JsonProperty("NfospreadBenefit")
+    private String nfospreadBenefit;
+    @JsonProperty("stCode")
+    private int stCode;
+    @JsonProperty("CollateralValue")
+    private String collateralValue;
+    @JsonProperty("AddMrgnNrmlPrsnt")
+    private String addMrgnNrmlPrsnt;
+    @JsonProperty("RmsCollateralMult")
+    private String rmsCollateralMult;
+    @JsonProperty("CncMrgnVarPrsnt")
+    private String cncMrgnVarPrsnt;
+    @JsonProperty("BoardLotLimit")
+    private int boardLotLimit;
+    @JsonProperty("ComRlsMtomPrsnt")
+    private String comRlsMtomPrsnt;
+    @JsonProperty("MarginVarPrsnt")
+    private String marginVarPrsnt;
+    @JsonProperty("DeliveryMarginPresent")
+    private String deliveryMarginPresent;
+    @JsonProperty("RmsCollateral")
+    private String rmsCollateral;
+    @JsonProperty("ComExpsrMrgnMisPrsnt")
+    private String comExpsrMrgnMisPrsnt;
+    @JsonProperty("CollateralValueMult")
+    private String collateralValueMult;
+    @JsonProperty("FoSpanrgnMisPrsnt")
+    private String foSpanrgnMisPrsnt;
+    @JsonProperty("Category")
+    private String category;
+    @JsonProperty("CurUnRlsMtomPrsnt")
+    private String curUnRlsMtomPrsnt;
+    @JsonProperty("SpanMarginPrsnt")
+    private String spanMarginPrsnt;
+    @JsonProperty("CncSellcrdPresent")
+    private String cncSellcrdPresent;
+    @JsonProperty("Collateral")
+    private String collateral;
+    @JsonProperty("MrgnScrpBsktNrmlPrsnt")
+    private String mrgnScrpBsktNrmlPrsnt;
+    @JsonProperty("UnrealizedMtomPrsnt")
+    private String unrealizedMtomPrsnt;
+    @JsonProperty("MrgnScrpBsktMisPrsnt")
+    private String mrgnScrpBsktMisPrsnt;
+    @JsonProperty("RealizedMtomPrsnt")
+    private String realizedMtomPrsnt;
+    @JsonProperty("MtomWarningPrcntPrsnt")
+    private String mtomWarningPrcntPrsnt;
+    @JsonProperty("ComSpanMrgnNrmlPrsnt")
+    private String comSpanMrgnNrmlPrsnt;
+    @JsonProperty("DeliveryMrgnNrmlPrsnt")
+    private String deliveryMrgnNrmlPrsnt;
+    @JsonProperty("AdhocLimitMult")
+    private String adhocLimitMult;
+    @JsonProperty("FoPremiumNrmlPrsnt")
+    private String foPremiumNrmlPrsnt;
+    @JsonProperty("SpecialMarginPrsnt")
+    private String specialMarginPrsnt;
+    @JsonProperty("SplMrgnNrmlPrsnt")
+    private String splMrgnNrmlPrsnt;
+    @JsonProperty("MtomSquareOffWarningPrcntPrsnt")
+    private String mtomSquareOffWarningPrcntPrsnt;
+    @JsonProperty("MarginUsedPrsnt")
+    private String marginUsedPrsnt;
+    @JsonProperty("FoRlsMtomPrsnt")
+    private String foRlsMtomPrsnt;
+    @JsonProperty("stat")
+    private String stat;
+    @JsonProperty("NationalCashMult")
+    private String nationalCashMult;
+    @JsonProperty("FoSpanrgnNrmlPrsnt")
+    private String foSpanrgnNrmlPrsnt;
+    @JsonProperty("AmtUntilizedPrsnt")
+    private String amtUntilizedPrsnt;
+    @JsonProperty("PremiumPrsnt")
+    private String premiumPrsnt;
+    @JsonProperty("AddMrgnMisPrsnt")
+    private String addMrgnMisPrsnt;
+    @JsonProperty("DeliveryMrgnMisPrsnt")
+    private String deliveryMrgnMisPrsnt;
+    @JsonProperty("CdsSpreadBenefit")
+    private String cdsSpreadBenefit;
+    @JsonProperty("ComExpsrMrgnNrmlPrsnt")
+    private String comExpsrMrgnNrmlPrsnt;
+    @JsonProperty("FoExpMrgnNrmlPrsnt")
+    private String foExpMrgnNrmlPrsnt;
+    @JsonProperty("BrokeragePrsnt")
+    private String brokeragePrsnt;
+    @JsonProperty("CashUnRlsMtomPrsnt")
+    private String cashUnRlsMtomPrsnt;
+    @JsonProperty("AddPreExpMrgnNrmlPrsnt")
+    private String addPreExpMrgnNrmlPrsnt;
+    @JsonProperty("CurSpanMrgnMisPrsnt")
+    private String curSpanMrgnMisPrsnt;
+    @JsonProperty("ComUnRlsMtomPrsnt")
+    private String comUnRlsMtomPrsnt;
+    @JsonProperty("CashRlsMtomPrsnt")
+    private String cashRlsMtomPrsnt;
+    @JsonProperty("NotionalCash")
+    private String notionalCash;
+    @JsonProperty("CurExpMrgnMisPrsnt")
+    private String curExpMrgnMisPrsnt;
+    @JsonProperty("MarginUsed")
+    private String marginUsed;
+    @JsonProperty("Net")
+    private String net;
+    @JsonProperty("TenderMrgnMisPrsnt")
+    private String tenderMrgnMisPrsnt;
+    @JsonProperty("FoExpMrgnMisPrsnt")
+    private String foExpMrgnMisPrsnt;
+    @JsonProperty("TenderMrgnNrmlPrsnt")
+    private String tenderMrgnNrmlPrsnt;
+    @JsonProperty("MarginScripBasketPrsnt")
+    private String marginScripBasketPrsnt;
+    @JsonProperty("ComSpanMrgnMisPrsnt")
+    private String comSpanMrgnMisPrsnt;
+}
