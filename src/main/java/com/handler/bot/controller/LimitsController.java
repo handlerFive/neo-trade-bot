179a50e1362a06492a18ca839a38a4874b59a095
handlerFive <raunaksarkar12@gmail.com>
2025-06-24 23:03:54 +0530

    Neo-Bot - Limits Fetching Fix

--- src/main/java/com/handler/bot/controller/LimitsController.java
+++ src/main/java/com/handler/bot/controller/LimitsController.java
@@ -1,44 +1,27 @@
 package com.handler.bot.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.handler.bot.model.LimitsRes;
 import com.handler.bot.service.LimitsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/limits")
 public class LimitsController {
     private final LimitsService limitsService;
     private static final Logger logger = LoggerFactory.getLogger(LimitsController.class);
-    Map<String, Object> requestBody = Map.of(
-            "seg", "CASH",
-            "exch", "NSE",
-            "prod", "ALL"
-    );
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public LimitsController(LimitsService limitsService) {
         this.limitsService = limitsService;
     }
 
-    @SuppressWarnings("unchecked")
     @PostMapping
     public LimitsRes getLimits() {
-        try {
-            String jsonRequestBody = objectMapper.writeValueAsString(requestBody);
-            LimitsRes limits = limitsService.fetchLimits(objectMapper.readValue(jsonRequestBody, Map.class));
-            logger.info("Limits Fetched : {}", limits);
-            return limits;
-        } catch (Exception e) {
-            logger.error("Error converting map to JSON", e);
-            return null;
-        }
+        LimitsRes limits = limitsService.fetchLimits();
+        logger.info("Limits Fetched");
+        return limits;
     }
 }
--- src/main/java/com/handler/bot/service/LimitsService.java
+++ src/main/java/com/handler/bot/service/LimitsService.java
@@ -6,8 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Map;
+import reactor.core.publisher.Mono;
 
 import static com.handler.bot.constants.Constant.API_SUB_KEY;
 import static com.handler.bot.constants.Constant.LIMITS_URL;
@@ -25,17 +24,24 @@ public class LimitsService {
         this.authService = authService;
     }
 
-    public LimitsRes fetchLimits(Map<String, Object> requestBody) {
+    public LimitsRes fetchLimits() {
+        String jData = "jData={\"seg\":\"CASH\",\"exch\":\"NSE\",\"prod\":\"ALL\"}";
         return webClient.post()
                 .uri(LIMITS_URL)
                 .header("Sid", sessionTokenStorage.getSid())
                 .header("Auth", sessionTokenStorage.getSessionToken())
                 .header("neo-fin-key", API_SUB_KEY)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
                 .header("Authorization", "Bearer " + authService.getAccessToken())
-                .bodyValue(requestBody)
-                .retrieve()
-                .bodyToMono(LimitsRes.class)
+                .bodyValue(jData)
+                .exchangeToMono(response -> {
+                    if (response.statusCode().isError()) {
+                        return response.bodyToMono(String.class)
+                                .doOnNext(errorBody -> System.err.println("Error: " + errorBody))
+                                .then(Mono.error(new RuntimeException("API error: " + response.statusCode())));
+                    }
+                    return response.bodyToMono(LimitsRes.class);
+                })
                 .block();
     }
 }
